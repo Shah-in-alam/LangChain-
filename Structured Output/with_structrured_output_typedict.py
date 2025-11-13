1724c50d05f8555d7e2d38558d7290fc89fae6e1
Mohammad Shahin Alam <shahinalam111024@gmail.com>
2025-11-13 12:04:49 +0100

    1. Create a bot. 2. Working on structured output. 3. Typedict librebies

---
+++ Structured Output/with_structrured_output_typedict.py
@@ -0,0 +1,19 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from typing import TypedDict,Annotated
+
+load_dotenv()
+model =ChatOpenAI()
+
+# Schema 
+class Review(TypedDict):
+    summary: Annotated[str,"A brief summary of the review"]
+    sentiment: Annotated[str,"Return sentiment of the review either negative, positive or netural"]
+
+structured_model=model.with_structured_output(Review)
+
+result=structured_model.invoke(""" The hardware is great, but the software feels bloated. There are too many pre-installed apps that I can;t remove. Also, the UI looks oudated copared to other brands. Hoping for a sowftware update to fix this.""")
+
+print(result)
+print(result['summary'])
+print(result['sentiment'])
