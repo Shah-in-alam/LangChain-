570fd2992d8adfc6ab10773cbf8f372ed6c8cd82
Mohammad Shahin Alam <shahinalam111024@gmail.com>
2025-11-16 19:03:01 +0100

    Just added the jsonoutputparser. how it works

---
+++ Output Parser/jsonoutputparser.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-3-4b-it",
+    task="text-generation",
+)
+
+model = ChatHuggingFace(llm=llm)
+parser = JsonOutputParser()
+
+template1 = PromptTemplate(
+    template='Give me the name, age, and city of a fictional character \n {format_instructions}',
+    input_variables=['format_instructions'],
+    partial_variables={'format_instructions': parser.get_format_instructions()}
+)
+
+# prompt = template1.format()
+# result = model.invoke(prompt)
+# parsed_output = parser.parse(result.content)  
+chain = template1 | model | parser
+output = chain.invoke({})
+print(output['name'])
+print(output['age'])
+print(output['city'])
+print(type(output))
+
--- Structured Output/with_structrured_output_typedict.py
+++ Structured Output/with_structrured_output_typedict.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from typing import TypedDict,Annotated,Optional
+from typing import TypedDict,Annotated,Optional,Literal
 
 load_dotenv()
 model =ChatOpenAI()
@@ -9,14 +9,16 @@ model =ChatOpenAI()
 class Review(TypedDict):
     key_themes: Annotated[str,"Write down all the key themes discussed in the review"] 
     summary: Annotated[str,"A brief summary of the review"]
-    sentiment: Annotated[str,"Return sentiment of the review either negative, positive or netural"]
+    sentiment: Annotated[Literal["pos","neg"],"Return sentiment of the review either negative, positive or netural"]
     pros: Annotated[Optional[list[str]],"Write down the pros mentioned in the review"]
     cons: Annotated[Optional[list[str]],"Write down the cons mentioned in the review"]
+    name: Annotated[Optional[str],"Name of the reviewer if mentioned in the review"]
 
 
 structured_model=model.with_structured_output(Review)
 
-result=structured_model.invoke(""" The overall experience with this platform has been impressive. The interface is clean and easy to navigate, and the performance is fast even during heavy tasks. However, customer support responses were slightly delayed, which affected the overall experience. Still, the value and features provided make it a strong choice for beginners and professionals alike.""")
+result=structured_model.invoke(""" The overall experience with this platform has been impressive. The interface is clean and easy to navigate, and the performance is fast even during heavy tasks. However, customer support responses were slightly delayed, which affected the overall experience. Still, the value and features provided make it a strong choice for beginners and professionals alike.
+                               - reviewer: Shahin Alam.""")
 
 print(result)
 print(result['summary'])
