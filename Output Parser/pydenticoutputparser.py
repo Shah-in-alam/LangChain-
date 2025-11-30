fac5713c291f6a1cdd0a5ee8d4dc735cea0db1b5
Mohammad Shahin Alam <shahinalam111024@gmail.com>
2025-11-30 01:12:42 +0100

    Pydentatic output parser

--- Output Parser/pydenticoutputparser.py
+++ Output Parser/pydenticoutputparser.py
@@ -17,3 +17,23 @@ class Person(BaseModel):
     name :str =Field(description='Name of the person')
     age: int =Field(description='Age of the person')
     city: str =Field(description='City of the person')  
+
+parser = PydanticOutputParser(pydantic_object=Person)
+template = PromptTemplate(
+    template="""Generate the name, age and city of a fictional {place} person
+     {format_instructions}""",
+    input_variables=['place'],
+    partial_variables={"format_instructions": parser.get_format_instructions()}
+)
+# prompt = template.iunvoke({'place': 'Italian'})
+# response = model.predict(prompt)
+# person = parser.parse(response)
+# print(person)
+chain = template | model | parser
+person = chain.invoke({'place': 'Italian'})
+print(person)
+    
+
+
+    
+  
