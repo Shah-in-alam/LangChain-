90d7604413f62a24b1615fa1e254711a9e0c656b
Mohammad Shahin Alam <shahinalam111024@gmail.com>
2025-12-29 01:59:52 +0100

    Document loader start

---
+++ Document_loader/text_loader.py
@@ -0,0 +1,31 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+prompt = PromptTemplate(
+    template='Summarize the following text \n {text}.',
+    input_variables=['text'],
+)
+
+parser = StrOutputParser()
+
+loader = TextLoader(
+    "LAGraph_Detailed_Understanding.txt",
+    encoding="utf-8",
+)
+
+docs = loader.load()
+
+print(docs[0].page_content[:1000])
+print(docs[0].metadata)   # should be of type Document
+
+chain =prompt | model | parser
+result= chain.invoke({'text': docs[0].page_content})
+print(result)
---
+++ Runnable_2/lcel.py
@@ -0,0 +1,8 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnablePassthrough,RunnableParallel,RunnableLambda,RunnableBranch
+
+
+load_dotenv()
